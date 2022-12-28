c88a6255b630ebafb4c08527eef42928f7f30362
lift0xff <lift0xff@outlook.com>
2022-12-28 12:58:23 +0700

    Fix router abi

--- src/abis.ts
+++ src/abis.ts
@@ -1,7 +1,6 @@
 export const abis = {
   '0_1': [
-    'function payWithToken(bytes32 _memo, uint256 _amount, address[] _priceFeeds, address _token, address _receiver)',
-    'function payWithToken(bytes32 _memo, uint256 _amount, address[] _priceFeeds, address _token, address _receiver, address _extraFeeReceiver, uint256 _extraFeeDivisor)',
+    'function payWithToken(bytes32 _memo, uint256 _amount, address[] _priceFeeds, address _token, address _receiver, address _extraFeeReceiver, uint256 _extraFeeDivisor) returns (bool)',
     'event Payment(address indexed sender, address indexed receiver, address token, uint256 amount, uint256 fees, bytes32 memo)',
     'event Convert(address indexed priceFeed, int256 price)',
   ],

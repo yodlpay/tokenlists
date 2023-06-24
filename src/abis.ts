88a64f45cf186fdc4f6c5143a770781dc40dcab5
ionica21 <ionicat@windowslive.com>
2023-06-24 14:34:51 +0200

    Use any type for ABIs

--- src/abis.ts
+++ src/abis.ts
@@ -1,5 +1,5 @@
 import HiroRouterV1Abi from "./abis/hirorouterv1.abi.json";
 import CurveRouterV13Abi from "./abis/curverouterv13.abi.json";
 
-export const HIRO_ROUTER_ABI = HiroRouterV1Abi;
-export const CURVE_ROUTER_ABI = CurveRouterV13Abi;
+export const HIRO_ROUTER_ABI: any = HiroRouterV1Abi;
+export const CURVE_ROUTER_ABI: any = CurveRouterV13Abi;

ccc3d57af2e994ec3dfd58bfa77267f0f502b065
ionica21 <ionicat@windowslive.com>
2023-08-02 16:46:59 +0200

    Add v0.1 router ABI, update v0.2 router ABI and have a key-value mapping for version to router abi

--- src/abis.ts
+++ src/abis.ts
@@ -1,5 +1,10 @@
-import HiroRouterV1Abi from "./abis/hirorouterv1.abi.json";
 import CurveRouterV13Abi from "./abis/curverouterv13.abi.json";
+import HiroRouterV01 from "./abis/HiroRouterV0.1.abi.json";
+import HiroRouterV02 from "./abis/HiroRouterV0.2.abi.json";
 
-export const HIRO_ROUTER_ABI = HiroRouterV1Abi;
+export const HIRO_ROUTER_ABIS: { [key: string]: any} = {
+  "0.1": HiroRouterV01,
+  "0.2": HiroRouterV02,
+  "test": HiroRouterV02
+}
 export const CURVE_ROUTER_ABI = CurveRouterV13Abi;

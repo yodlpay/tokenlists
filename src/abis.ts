f91beb7ea036328ea86e72b86eeeb383dfd94bb2
sam <samuel@yodlpay.com>
2024-06-21 11:11:24 +0200

    YODL-357 - rename HiroRouter to YodlRouter

--- src/abis.ts
+++ src/abis.ts
@@ -1,12 +1,12 @@
 import CurveRouterV13Abi from "./abis/curverouterv13.abi.json";
-import HiroRouterV01 from "./abis/HiroRouterV0.1.abi.json";
-import HiroRouterV02 from "./abis/HiroRouterV0.2.abi.json";
-import HiroRouterV03 from "./abis/HiroRouterV0.3.abi.json";
+import YodlRouterV01 from "./abis/YodlRouterV0.1.abi.json";
+import YodlRouterV02 from "./abis/YodlRouterV0.2.abi.json";
+import YodlRouterV03 from "./abis/YodlRouterV0.3.abi.json";
 
 export const HIRO_ROUTER_ABIS: { [key: string]: any} = {
-  "0.1": HiroRouterV01,
-  "0.2": HiroRouterV02,
-  "0.3": HiroRouterV03,
-  "test": HiroRouterV03
+  "0.1": YodlRouterV01,
+  "0.2": YodlRouterV02,
+  "0.3": YodlRouterV03,
+  "test": YodlRouterV03
 }
 export const CURVE_ROUTER_ABI = CurveRouterV13Abi;

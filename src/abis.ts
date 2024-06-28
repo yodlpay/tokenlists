0605b0c6e639ddadc96f37bd8ff5a8c27dbdc3ae
sam <samuel@yodlpay.com>
2024-06-28 14:52:11 +0200

    YODL-383 - add new 0.4 version of YodlRouter with support of Curve and YD metadata tracking

--- src/abis.ts
+++ src/abis.ts
@@ -2,11 +2,13 @@ import CurveRouterV13Abi from "./abis/curverouterv13.abi.json";
 import YodlRouterV01 from "./abis/YodlRouterV0.1.abi.json";
 import YodlRouterV02 from "./abis/YodlRouterV0.2.abi.json";
 import YodlRouterV03 from "./abis/YodlRouterV0.3.abi.json";
+import YodlRouterV04 from "./abis/YodlRouterV0.4.abi.json";
 
-export const HIRO_ROUTER_ABIS: { [key: string]: any} = {
+export const YODL_ROUTER_ABIS: { [key: string]: any} = {
   "0.1": YodlRouterV01,
   "0.2": YodlRouterV02,
   "0.3": YodlRouterV03,
+  "0.4": YodlRouterV04,
   "test": YodlRouterV03
 }
 export const CURVE_ROUTER_ABI = CurveRouterV13Abi;
